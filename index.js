616a9b43eedabeeb33eabf590753384862809509
Liam Shaw <liamjosephshaw@gmail.com>
2024-10-04 19:44:24 +0100

    Removed limiting

--- index.js
+++ index.js
@@ -82,11 +82,11 @@ app.set('trust proxy', 1);
 // }));
 
 // Set up rate limiting
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: "Too many requests from this IP, please try again later."
-});
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//     message: "Too many requests from this IP, please try again later."
+// });
 
 app.use(limiter);
 
